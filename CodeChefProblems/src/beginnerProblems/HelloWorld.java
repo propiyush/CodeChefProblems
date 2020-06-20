267947c6fea7b1385b8852a1ceb843701016380c
piyushjindal <piyushjindal@192.168.0.7>
2020-06-20 11:22:51 +0530

    First Commit

---
+++ CodeChefProblems/src/beginnerProblems/HelloWorld.java
@@ -0,0 +1,10 @@
+package beginnerProblems;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
